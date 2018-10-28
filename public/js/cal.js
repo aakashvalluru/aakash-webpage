a09f78983a2e9a0bbafc5d44be0ae875db094d6e
aakash <s533714@nwmissouri.edu>
2018-10-28 18:13:54 -0500

    heroku

--- public/js/cal.js
+++ public/js/cal.js
@@ -1,7 +1,19 @@
 function wage(){
 
 let hour = document.getElementById("hour").value;
+
+let price = document.getElementById("price").value;
+
+var today = new Date();
+var month = today.getMonth();
+let monthly = hour*price*4;
+let intrest = (monthly*2.5)/(100);
+let a =  "Number of hours : "+hour+"Amount for hour: "+price+"Monthly amount: "+monthly+"Intrest rate 2.5%: "+intrest+"dollors";
+document.getElementById("abc").innerHTML = "Number of hours : "+hour+"<br>Amount for hour: "+price+"<br>Monthly amount: "+monthly+"<br>Intrest rate 2.5%: "+intrest;
+alert("Number of hours : "+hour+"\nAmount for hour: "+price+"\nMonthly amount: "+monthly+"\nIntrest rate 2.5%: "+intrest);
+responsiveVoice.speak(a);
 wagecheck(hour);
+
 
 }
 //reference from responsivevoice.js api
@@ -12,17 +24,6 @@ function wagecheck(hour)
 		throw Error('The given argument is not a number');
 	}
 
-
-    let price = $("#price").value;
-
-var today = new Date();
-var month = today.getMonth();
-let monthly = hour*price*4;
-let intrest = (monthly*2.5)/(100);
-let a =  "Number of hours : "+hour+"Amount for hour: "+price+"Monthly amount: "+monthly+"Intrest rate 2.5%: "+intrest+"dollors";
-document.getElementById("abc").innerHTML = "Number of hours : "+hour+"<br>Amount for hour: "+price+"<br>Monthly amount: "+monthly+"<br>Intrest rate 2.5%: "+intrest;
-alert("Number of hours : "+hour+"\nAmount for hour: "+price+"\nMonthly amount: "+monthly+"\nIntrest rate 2.5%: "+intrest);
-responsiveVoice.speak(a);
 
 
 }
